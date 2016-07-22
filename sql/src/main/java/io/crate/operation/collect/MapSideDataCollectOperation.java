d0ae0dcf98c8302f273420dfae4bfb6aadbd529e
Christian Haudum <christian.haudum@gmail.com>
2016-07-22 22:33:25 +0200

    implement nodeOperation for nodestats action

--- sql/src/main/java/io/crate/operation/collect/MapSideDataCollectOperation.java
+++ sql/src/main/java/io/crate/operation/collect/MapSideDataCollectOperation.java
@@ -101,10 +101,9 @@ public class MapSideDataCollectOperation {
             RoutedCollectPhase routedCollectPhase = (RoutedCollectPhase) collectPhase;
             switch (routedCollectPhase.maxRowGranularity()) {
                 case NODE:
-                    break;
                 case DOC:
                     EvaluatingNormalizer normalizer =
-                            new EvaluatingNormalizer(functions, RowGranularity.NODE, new NodeSysReferenceResolver(nodeSysExpression));
+                        new EvaluatingNormalizer(functions, RowGranularity.DOC, new NodeSysReferenceResolver(nodeSysExpression));
                     return collectPhase.normalize(normalizer, null);
             }
         }
@@ -116,8 +115,8 @@ public class MapSideDataCollectOperation {
         Executor executor = threadPool.executor(threadPoolName);
         if (executor instanceof ThreadPoolExecutor) {
             ThreadPools.runWithAvailableThreads(
-                    (ThreadPoolExecutor) executor,
-                    collectors2Runnables(shardCollectors));
+                (ThreadPoolExecutor) executor,
+                collectors2Runnables(shardCollectors));
         } else {
             // assume executor is just a wrapper to 1 thread
             for (CrateCollector collector : shardCollectors) {
