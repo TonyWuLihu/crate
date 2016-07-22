2eafd67b18b1718c889df1f6a329f9571cb989f9
Christian Haudum <christian.haudum@gmail.com>
2016-07-22 22:08:54 +0200

    fixed wrong ident in reference resolver

--- sql/src/main/java/io/crate/operation/reference/sys/RowContextReferenceResolver.java
+++ sql/src/main/java/io/crate/operation/reference/sys/RowContextReferenceResolver.java
@@ -78,7 +78,7 @@ public class RowContextReferenceResolver implements ReferenceResolver<RowCollect
         tableFactories.put(InformationColumnsTableInfo.IDENT, InformationSchemaExpressionFactories.columnsFactories());
         tableFactories.put(InformationTablesTableInfo.IDENT, InformationSchemaExpressionFactories.tablesFactories());
 
-        tableFactories.put(SysNodeChecksTableInfo.IDENT, SysNodesExpressionFactories.getSysNodesTableInfoFactories());
+        tableFactories.put(SysNodesTableInfo.IDENT, SysNodesExpressionFactories.getSysNodesTableInfoFactories());
         tableFactories.put(SysShardsTableInfo.IDENT, UnassignedShardsExpressionFactories.getSysShardsTableInfoFactories());
 
         tableFactories.put(PgTypeTable.IDENT, PgCatalogTables.pgTypeExpressions());
